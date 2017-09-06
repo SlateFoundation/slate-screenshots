21e065120a1f6dcd500e13495f55433f4e358e68
Chris Alfano <chris@jarv.us>
2017-09-06 19:21:58 -0400

    Add initial capture of slate-develop

---
+++ slate-develop/capture.js
@@ -0,0 +1,5 @@
+require('../lib').capture(__dirname, {
+    'homepage': 'slate-develop.dev01.slatepowered.net',
+    'dashboard': 'slate-develop.dev01.slatepowered.net/dashboard',
+    'section': 'slate-develop.dev01.slatepowered.net/sections/ELA-001'
+});
