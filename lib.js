786cfa77ed29f1084e7a1e53ee39ae7d93516566
Chris Alfano <chris@jarv.us>
2017-09-06 19:31:09 -0400

    Initial commit of capture tool

---
+++ lib.js
@@ -0,0 +1,26 @@
+const Pageres = require('pageres');
+
+exports.capture = function(destination, urls) {
+    const sizes = ['1366x768'];
+    const sessionToken = (process.argv[2]||'').trim();
+
+    if (!sessionToken) {
+        console.log('Warning: no session token provided');
+    }
+
+    const pageres = new Pageres({
+        cookies: [{
+            name: 's_sla-cc-live',
+            value: sessionToken
+        }],
+        filename: '<%= url %>'
+    });
+
+    Object.keys(urls).forEach((page) => pageres.src(urls[page], sizes, {filename: page}));
+
+    pageres.dest(destination).run().then(function() {
+        console.log('done');
+    }).catch(function(error) {
+        console.log('failed:', error);
+    });
+};
