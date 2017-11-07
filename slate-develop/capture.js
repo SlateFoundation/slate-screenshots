f2e8b215d95d74d770cb852c8135d92a3eb93233
Chris Alfano <chris@jarv.us>
2017-11-06 21:35:53 -0500

    Add manage and cms authoring pages to capture

--- slate-develop/capture.js
+++ slate-develop/capture.js
@@ -1,5 +1,29 @@
 require('../lib').capture(__dirname, 'http://slate-develop.dev01.slatepowered.net', {
     'homepage': '/',
     'dashboard': '/dashboard',
-    'section': '/sections/ELA-001'
+    'section': '/sections/ELA-001',
+
+    'manage-people': {
+        path: '/manage#people/search/group:class_of_2019/aabernathy/profile',
+        waitFor: '[id^="people-grid-"] .x-grid-row'
+    },
+
+    'manage-section-profile': '/manage#course-sections/lookup/ELA-001/profile',
+    'manage-section-participants': '/manage#course-sections/lookup/ELA-001/participants',
+
+    'manage-settings-groups': '/manage#settings/groups',
+    'manage-settings-terms': '/manage#settings/terms',
+    'manage-settings-departments': '/manage#settings/departments',
+    'manage-settings-courses': '/manage#settings/courses',
+
+    'manage-progress-interims-report': '/manage#progress/interims/report',
+    'manage-progress-interims-export': '/manage#progress/interims/print',
+    'manage-progress-interims-email': '/manage#progress/interims/email',
+
+    'manage-progress-terms-report': '/manage#progress/terms/report',
+    'manage-progress-terms-export': '/manage#progress/terms/print',
+    'manage-progress-terms-email': '/manage#progress/terms/email',
+
+    'cms-page-create': '/pages/create?Title=Test+Page',
+    'cms-blog-create': '/blog/create?Title=Test+Blog+Post'
 });
